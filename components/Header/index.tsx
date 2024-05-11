ab6a0fb4508151edc2d767751021b97cada3e371
Subashvel <srsvel2000@gmail.com>
2024-05-11 23:22:30 +0530

    Updated code to include new feature

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -28,7 +28,7 @@ const Header = () => {
 
   // submenu handler
   const [openIndex, setOpenIndex] = useState(-1);
-  const handleSubmenu = (index) => {
+  const handleSubmenu = (index:number) => {
     if (openIndex === index) {
       setOpenIndex(-1);
     } else {
@@ -131,9 +131,9 @@ const Header = () => {
                                 openIndex === index ? "block" : "hidden"
                               }`}
                             >
-                              {menuItem.submenu.map((submenuItem, index) => (
+                              {menuItem.submenu?.map((submenuItem, index) => (
                                 <Link
-                                  href={submenuItem.path}
+                                  href={submenuItem.path || ''}
                                   key={index}
                                   className="block rounded py-2.5 text-sm text-dark hover:text-[#45988e] dark:text-white/70 dark:hover:text-white lg:px-3"
                                 >
