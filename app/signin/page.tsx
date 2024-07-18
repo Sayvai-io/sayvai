4fd1a996fc512d927c0b60f70d462a468f5d8cf1
sayvai <prakash@sayvai.io>
2024-07-18 19:47:09 +0530

    Remove the signIn with Github button

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Sign In Page | Free Next.js Template for Startup and SaaS",
-  description: "This is Sign In Page for Startup Nextjs Template",
+  title: "Sign In Page",
+  // description: "",
   // other metadata
 };
 
@@ -16,38 +16,13 @@ const SigninPage = () => {
           <div className="-mx-4 flex flex-wrap">
             <div className="w-full px-4">
               <div className="shadow-three mx-auto max-w-[500px] rounded bg-white px-6 py-10 dark:bg-dark sm:p-[60px]">
-              
-                <h3 className="mb-3 text-center text-2xl font-bold text-black dark:text-white sm:text-3xl">
+                <h3 className="mb-3 text-center text-2xl font-bold text-[#16C3A6] dark:text-[#16C3A6] sm:text-3xl">
                   Sign in to your account
                 </h3>
                 <p className="mb-11 text-center text-base font-medium text-body-color">
                   Login to your account for a faster checkout.
                 </p>
-                
-                <div className="border-b border-[#45988e] mb-6">
-                <div className="mb-6 grid grid-cols-2 text-center">
-
-                 
-                  <Link href="signin" className="text-sm font-medium text-[#45988e] hover:underline">
-                  <button className="shadow-submit dark:shadow-submit-dark flex w-full items-center justify-center rounded-sm bg-[#45988e] px-9 py-4 text-base font-medium text-white duration-300 hover:bg-[#45988e]/90">
-                      Signin
-                  </button>
-                  </Link>
-                  
-                 
-                  <Link href="/signup" className="text-sm font-medium text-[#45988e] hover:underline">
-                    
-                  <button className="shadow-submit dark:shadow-submit-dark flex w-full items-center justify-center rounded-sm bg-[#45988e] px-9 py-4 text-base font-medium text-white duration-300 hover:bg-[#45988e]/90">
-                      Signup
-                  </button>
-                    
-                  </Link>
-                  
-                </div>
-
-                </div>
-
-                <button className="border-stroke dark:text-body-color-dark dark:shadow-two mb-6 flex w-full items-center justify-center rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 hover:border-primary hover:bg-primary/5 hover:text-[#45988e] dark:border-transparent dark:bg-[#2C303B] dark:hover:border-[#45988e] dark:hover:bg-primary/5 dark:hover:text-[#45988e] dark:hover:shadow-none">
+                <button className="border-stroke dark:text-body-color-dark dark:shadow-two mb-6 flex w-full items-center justify-center rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 hover:border-[#16C3A6] hover:bg-primary/5 hover:text-[#16C3A6] dark:border-transparent dark:bg-[#2C303B] dark:hover:border-[#16C3A6] dark:hover:bg-primary/5 dark:hover:text-[#16C3A6] dark:hover:shadow-none">
                   <span className="mr-3">
                     <svg
                       width="20"
@@ -84,7 +59,7 @@ const SigninPage = () => {
                   Sign in with Google
                 </button>
 
-                <button className="border-stroke dark:text-body-color-dark dark:shadow-two mb-6 flex w-full items-center justify-center rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 hover:border-[#45988e] hover:bg-primary/5 hover:text-[#45988e] dark:border-transparent dark:bg-[#2C303B] dark:hover:border-[#45988e] dark:hover:bg-primary/5 dark:hover:text-[#45988e] dark:hover:shadow-none">
+                {/* <button className="border-stroke dark:text-body-color-dark dark:shadow-two mb-6 flex w-full items-center justify-center rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 hover:border-primary hover:bg-primary/5 hover:text-primary dark:border-transparent dark:bg-[#2C303B] dark:hover:border-primary dark:hover:bg-primary/5 dark:hover:text-primary dark:hover:shadow-none">
                   <span className="mr-3">
                     <svg
                       fill="currentColor"
@@ -97,7 +72,7 @@ const SigninPage = () => {
                     </svg>
                   </span>
                   Sign in with Github
-                </button>
+                </button> */}
                 <div className="mb-8 flex items-center justify-center">
                   <span className="hidden h-[1px] w-full max-w-[70px] bg-body-color/50 sm:block"></span>
                   <p className="w-full px-5 text-center text-base font-medium text-body-color">
@@ -117,7 +92,7 @@ const SigninPage = () => {
                       type="email"
                       name="email"
                       placeholder="Enter your Email"
-                      className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-[#45988e] dark:border-transparent dark:bg-[#2C303B] dark:focus:border-[#45988e] dark:focus:shadow-none"
+                      className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-[#16C3A6] dark:border-transparent dark:bg-[#2C303B] dark:focus:border-[#16C3A6] dark:focus:shadow-none"
                     />
                   </div>
                   <div className="mb-8">
@@ -131,7 +106,7 @@ const SigninPage = () => {
                       type="password"
                       name="password"
                       placeholder="Enter your Password"
-                      className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:focus:border-primary dark:focus:shadow-none"
+                      className="border-stroke dark:text-body-color-dark dark:shadow-two w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none transition-all duration-300 focus:border-[#16C3A6] dark:border-transparent dark:bg-[#2C303B] dark:focus:border-[#16C3A6] dark:focus:shadow-none"
                     />
                   </div>
                   <div className="mb-8 flex flex-col justify-between sm:flex-row sm:items-center">
@@ -157,8 +132,8 @@ const SigninPage = () => {
                               >
                                 <path
                                   d="M10.0915 0.951972L10.0867 0.946075L10.0813 0.940568C9.90076 0.753564 9.61034 0.753146 9.42927 0.939309L4.16201 6.22962L1.58507 3.63469C1.40401 3.44841 1.11351 3.44879 0.932892 3.63584C0.755703 3.81933 0.755703 4.10875 0.932892 4.29224L0.932878 4.29225L0.934851 4.29424L3.58046 6.95832C3.73676 7.11955 3.94983 7.2 4.1473 7.2C4.36196 7.2 4.55963 7.11773 4.71406 6.9584L10.0468 1.60234C10.2436 1.4199 10.2421 1.1339 10.0915 0.951972ZM4.2327 6.30081L4.2317 6.2998C4.23206 6.30015 4.23237 6.30049 4.23269 6.30082L4.2327 6.30081Z"
-                                  fill="#3056D3"
-                                  stroke="#3056D3"
+                                  fill="#16C3A6"
+                                  stroke="#16C3A6"
                                   strokeWidth="0.4"
                                 />
                               </svg>
@@ -171,21 +146,21 @@ const SigninPage = () => {
                     <div>
                       <a
                         href="#0"
-                        className="text-sm font-medium text-[#45988e] hover:underline"
+                        className="text-sm font-medium text-[#16C3A6] hover:underline"
                       >
                         Forgot Password?
                       </a>
                     </div>
                   </div>
                   <div className="mb-6">
-                    <button className="shadow-submit dark:shadow-submit-dark flex w-full items-center justify-center rounded-sm bg-[#45988e] px-9 py-4 text-base font-medium text-white duration-300 hover:bg-[#45988e]/90">
+                    <button className="shadow-submit dark:shadow-submit-dark flex w-full items-center justify-center rounded-sm bg-[#16C3A6] px-9 py-4 text-base font-medium text-white duration-300 hover:bg-[#16C3A6]/90">
                       Sign in
                     </button>
                   </div>
                 </form>
                 <p className="text-center text-base font-medium text-body-color">
                   Don’t you have an account?{" "}
-                  <Link href="/signup" className="text-[#45988e] hover:underline">
+                  <Link href="/signup" className="text-[#16C3A6] hover:underline">
                     Sign up
                   </Link>
                 </p>
@@ -210,7 +185,7 @@ const SigninPage = () => {
               width="1440"
               height="969"
             >
-              <rect width="1440" height="969" fill="#090E34" />
+              <rect width="1440" height="969" fill="#16C3A6" />
             </mask>
             <g mask="url(#mask0_95:1005)">
               <path
@@ -233,8 +208,8 @@ const SigninPage = () => {
                 y2="453.581"
                 gradientUnits="userSpaceOnUse"
               >
-                <stop stopColor="#4A6CF7" />
-                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+                <stop stopColor="#16C3A6" />
+                <stop offset="1" stopColor="#16C3A6" stopOpacity="0" />
               </linearGradient>
               <linearGradient
                 id="paint1_linear_95:1005"
@@ -244,8 +219,8 @@ const SigninPage = () => {
                 y2="1192.04"
                 gradientUnits="userSpaceOnUse"
               >
-                <stop stopColor="#4A6CF7" />
-                <stop offset="1" stopColor="#4A6CF7" stopOpacity="0" />
+                <stop stopColor="#16C3A6" />
+                <stop offset="1" stopColor="#16C3A6" stopOpacity="0" />
               </linearGradient>
             </defs>
           </svg>
